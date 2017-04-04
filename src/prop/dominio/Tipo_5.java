b81fb45a3f1497b13da12161feff983a0801ebf5
micky101224 <micky101224@gmail.com>
2017-04-04 11:46:36 +0200

    Update Tipo_5.java

--- src/prop/dominio/Tipo_5.java
+++ src/prop/dominio/Tipo_5.java
@@ -1,5 +1,15 @@
 package prop.dominio;
 public class Tipo_5 extends Pregunta {
 	
+	public Tipo_5(){
+		super();
+		tipo = 5;
+	}
+	
+	public void imprimir_pregunta() {
+		System.out.println(id+"."+enunciado);
+	}
+	
 	
 }
+
