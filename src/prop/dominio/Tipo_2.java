44247a56dab748f6304567b6410f1daf720119f0
micky101224 <micky101224@gmail.com>
2017-04-04 11:45:01 +0200

    Update Tipo_2.java

--- src/prop/dominio/Tipo_2.java
+++ src/prop/dominio/Tipo_2.java
@@ -3,9 +3,21 @@ import java.util.ArrayList;
 
 public class Tipo_2 extends Pregunta {
 	
-	int opciones;
+	Integer opciones;
 	ArrayList <String>lista_opciones;
 	
+	public Tipo_2(){
+		super();
+		tipo = 2;
+		lista_opciones = new ArrayList <String>();
+	}
+	
+	public Tipo_2(Integer id, String enunciado, Integer opciones, ArrayList <String>lista_opciones){
+		super(id,enunciado);
+		tipo = 1;
+		this.opciones = opciones;
+		this.lista_opciones = lista_opciones;
+	}	
 	public int getOpciones() {
 		return opciones;
 	}
@@ -17,6 +29,16 @@ public class Tipo_2 extends Pregunta {
 	}
 	public void setLista_opciones(ArrayList<String> lista_opciones) {
 		this.lista_opciones = lista_opciones;
+	}
+	
+	public void añadir_opcion(String s){
+		lista_opciones.add(s);
+	
+	}
+	
+	public void imprimir_pregunta(){
+		System.out.println(id+"."+enunciado);
+		for (int i = 0; i < opciones; ++i) System.out.println("- "+ lista_opciones.get(i));
 	}
 
 }
