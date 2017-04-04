d8a61f099d74db6439ad52e39f5adfdb02cc4096
micky101224 <micky101224@gmail.com>
2017-04-04 11:45:55 +0200

    Update Tipo_4.java

--- src/prop/dominio/Tipo_4.java
+++ src/prop/dominio/Tipo_4.java
@@ -6,6 +6,19 @@ public class Tipo_4 extends Pregunta {
 	int opciones;
 	ArrayList <String>lista_opciones;
 	
+	public Tipo_4(){
+		super();
+		tipo = 4;
+		lista_opciones = new ArrayList <String>();
+	}
+	
+	public Tipo_4(Integer id, String enunciado, Integer opciones, ArrayList <String>lista_opciones){
+		super(id,enunciado);
+		tipo = 1;
+		this.opciones = opciones;
+		this.lista_opciones = lista_opciones;
+	}	
+	
 	public int getOpciones() {
 		return opciones;
 	}
@@ -17,5 +30,14 @@ public class Tipo_4 extends Pregunta {
 	}
 	public void setLista_opciones(ArrayList<String> lista_opciones) {
 		this.lista_opciones = lista_opciones;
+	}
+	public void añadir_opcion(String s){
+		lista_opciones.add(s);
+	
+	}
+	
+	public void imprimir_pregunta(){
+		System.out.println(id+"."+enunciado);
+		for (int i = 0; i < opciones; ++i) System.out.println("- "+ lista_opciones.get(i));
 	}
 }
