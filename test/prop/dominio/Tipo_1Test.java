d67aa078acb47bbe89cecdf91ef03fa661710719
raffiot <raphael.madillo@epfl.ch>
2017-04-12 10:47:58 +0200

    Test for different type of question added

--- test/prop/dominio/Tipo_1Test.java
+++ test/prop/dominio/Tipo_1Test.java
@@ -2,6 +2,22 @@ package prop.dominio;
 
 import static org.junit.Assert.*;
 
-public class Tipo_1Test {
+import org.junit.Test;
 
+public class Tipo_1Test {
+	
+	@Test
+	public void generateAnswerTest(){
+		Tipo_1 t1 = new Tipo_1(0,"",1,2000,0);
+		RespuestaPregunta rp = t1.generateAnswer();
+		boolean b = rp.getValueR1() <= 2000 && rp.getValueR1() >= 0;
+		assertTrue(b);
+		
+		Tipo_1 t2 = new Tipo_1(1,"",1,200,-200);
+		RespuestaPregunta rp2 = t2.generateAnswer();
+		boolean b2 = rp2.getValueR1() <= 200 && rp2.getValueR1() >= -200;
+		assertTrue(b2);
+	}
+	
+	
 }
---
+++ test/prop/dominio/Tipo_3Test.java
@@ -0,0 +1,25 @@
+package prop.dominio;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class Tipo_3Test {
+	
+	@Test
+	public void generateAnswerTest(){
+		ArrayList<String> array = new ArrayList<>();
+		array.add("verde");
+		array.add("amarillo");
+		array.add("rojo");
+		array.add("azul");
+		array.add("negro");
+		
+		Tipo_3 t = new Tipo_3(0,"",5,array);
+		RespuestaPregunta rp = t.generateAnswer();
+		assertTrue(array.contains(rp.getValueR3()));
+	}
+
+}
