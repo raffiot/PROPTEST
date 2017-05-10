516fb0b79ac9133b5f695378238c49975688438e
raffiot <raphael.madillo@epfl.ch>
2017-05-10 17:09:29 +0200

    persistencia 2

--- src/dominio/clases/Conjunto_User.java
+++ src/dominio/clases/Conjunto_User.java
@@ -1,35 +1,59 @@
 package dominio.clases;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import persistencia.Persistencia;
+
 public class Conjunto_User {
-	Map<String,String> user_pass;
-	Map<String,String> user_type;
+	List<String> users;
+	List<String> passwords;
+	List<String> type;
+	final String pathUser = "Data/user";
+	final String pathPass = "Data/path";
+	final String pathType = "Data/type";
 	
-	public void generateMaps(){
-		//get from binary to fill maps
+	
+	@SuppressWarnings("unchecked")
+	public void leerMaps(){	
+		users = (ArrayList<String>) Persistencia.leer(pathUser);
+		passwords = (ArrayList<String>) Persistencia.leer(pathPass);
+		type = (ArrayList<String>) Persistencia.leer(pathType);
+	}
+	
+	public void guardarMaps(){
+		
+		Persistencia.escribir(users, pathUser);
+		Persistencia.escribir(users, pathPass);
+		Persistencia.escribir(users, pathType);
 	}
 	
 	public boolean yaExiste(String username){
-		return user_pass.containsKey(username);
+		return users.contains(username);
 	}
 	
 	public void addUser(String username, String password, String type){
-		user_pass.put(username, password);
-		user_type.put(username, type);
+		users.add(username);
+		passwords.add(password);
+		this.type.add(type);
 	}
 	
 	public void removeUser(String username){
-		user_pass.remove(username);
-		user_type.remove(username);
+		int index = users.indexOf(username);
+		users.remove(index);
+		passwords.remove(index);
+		type.remove(index);
 	}
 	
 	public boolean identification(String username, String password){
-		return user_pass.get(username).equals(password);
+		int index = users.indexOf(username);
+		return passwords.get(index).equals(password);
 	}
 	
 	public String getType(String username){
-		return user_type.get(username);
+		int index = users.indexOf(username);
+		return type.get(index);
 	}
 	
 }
