beb40bd088dfdfc964fbb6e613e58064e80e11ef
marinamoreno <lacasitos612>
2017-06-01 01:54:18 +0200

    qwer

--- src/presentacion/Frame_contestar.java
+++ src/presentacion/Frame_contestar.java
@@ -1,12 +1,9 @@
 package presentacion;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -15,24 +12,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
-import org.w3c.dom.events.MouseEvent;
-
-import com.sun.javafx.geom.Rectangle;
-
 import dominio.clases.Pregunta;
-import dominio.clases.RespuestaPregunta;
-import dominio.clases.Respuesta_1;
 import dominio.clases.Tipo_1;
-
-import javax.swing.BoxLayout;
 import javax.swing.JTextField;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JSpinner;
 import javax.swing.JCheckBox;
 import javax.swing.event.ChangeListener;
@@ -40,6 +26,10 @@ import javax.swing.event.ChangeEvent;
 
 public class Frame_contestar extends JFrame implements ActionListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JButton btnSalir;
 	private Controlador_presentacion cp;
@@ -50,7 +40,11 @@ public class Frame_contestar extends JFrame implements ActionListener{
     private int y = 249;
     private int z = 108;
     private int w = 16;
-    private int sp1 = 0;
+    private Integer sp1 = 0;
+    private String sp2;
+    private String sp3;
+    ArrayList<Integer> selected = new ArrayList<Integer>();
+    ArrayList<String> items = new ArrayList<String>();
     private JSpinner spinner;
 	
 	/**
@@ -82,61 +76,6 @@ public class Frame_contestar extends JFrame implements ActionListener{
 		this.cp = cp;
 		initialize();
 		
-	}
-	
-	public void tipo1(Integer x_bis, Integer index){
-
-        //return (int)spinner.getValue();
-	}
-	
-	public void tipo2(Integer x_bis, Integer index){
-		ArrayList<String> opcions = cp.getopcionest2(index);
-		SpinnerListModel model = new SpinnerListModel(opcions);
-		JSpinner spinner = new JSpinner(model);
-        spinner.setBounds(x_bis, y, z, w);
-        spinner.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				String res = (String)spinner.getValue();
-				System.out.println("SPINNER 2 :"+ res);
-			}
-		});
-        contentPane.add(spinner);
-        
-	}
-	
-	public void tipo3(Integer x_bis, Integer index){
-		ArrayList<String> opcions = cp.getopcionest3(index);
-		SpinnerListModel model = new SpinnerListModel(opcions);
-		JSpinner spinner = new JSpinner(model);
-        spinner.setBounds(x_bis, y, z, w);
-        contentPane.add(spinner);
-	}
-	
-	public void tipo4(Integer x_bis,Integer index){
-	    /*String[] strs = { "swing", "home", "basic", "metal", "JList" };
-
-	    final JList list = new JList(strs);
-
-	    list.setCellRenderer(new ListCellRenderer());
-	    list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	    list.setBorder(new EmptyBorder(0, 4, 0, 0));
-	    list.addMouseListener(new MouseAdapter() {
-	      public void mouseClicked(MouseEvent e) {
-	        int index = list.locationToIndex(e.getPoint());
-	        CheckableItem item = (CheckableItem) list.getModel()
-	            .getElementAt(index);
-	        item.setSelected(!item.isSelected());
-	        java.awt.Rectangle rect = list.getCellBounds(index, index);
-	        list.repaint(rect);
-	      }
-	    });*/
-	}
-	
-	public void tipo5(Integer x_bis,Integer index){
-		textField = new JTextField();
-        textField.setBounds(x_bis, y, z, w);
-        contentPane.add(textField);
-        textField.setColumns(10);
 	}
 	
 	
@@ -173,10 +112,8 @@ public class Frame_contestar extends JFrame implements ActionListener{
         scrollPane.setViewportView(textArea);
         
         //cada pos de los arrays es la misma pregunta-respuesta 
-        ArrayList<Integer> tiposPreg = new ArrayList<Integer>();
-        ArrayList<String> respPreg = new ArrayList<String>();
-        
-        int x_bis = x;
+        int[] tiposPreg = new int[cp.getnpreguntas()];
+        String[] respPreg = new String[cp.getnpreguntas()];
         
         for (int i = 0; i < cp.getnpreguntas(); ++i){
         	y += 20;
@@ -185,7 +122,7 @@ public class Frame_contestar extends JFrame implements ActionListener{
         	
         	int tipo = cp.gettipo(i); 
         	
-            JLabel lblNewLabel = new JLabel("Respesta " + (i+1) + ": ");
+            JLabel lblNewLabel = new JLabel("Respuesta " + (i+1) + ": ");
             lblNewLabel.setBounds(x, y, z, w);
             contentPane.add(lblNewLabel);
             
@@ -193,6 +130,9 @@ public class Frame_contestar extends JFrame implements ActionListener{
             System.out.println("tipo: " + tipo);
             
         	if (tipo == 1) {
+        		int index = i;
+        		tiposPreg[i] = tipo;
+        		
         		int min = cp.getminOpt1(i);
         		int max = cp.getmaxOpt1(i);
         		System.out.println("--------------------------" + min + " MAX: " + max);
@@ -204,27 +144,153 @@ public class Frame_contestar extends JFrame implements ActionListener{
                 spinner.addChangeListener(new ChangeListener() {
         			public void stateChanged(ChangeEvent e) {
         				sp1 = (int)spinner.getValue();
-        				System.out.println("SPINNER 1 :"+ sp1);
+        				respPreg[index] = sp1.toString(); //comprobar
+        				 System.out.println(respPreg[index]);
+        				//System.out.println("SPINNER 1 :"+ respPreg[index]);
         			}
         		});
                 contentPane.add(spinner);
                 
         	}
-        	else if (tipo == 2) tipo2(x_bis+120,i);
-        	else if (tipo == 3) tipo3(x_bis+120,i);
-        	else if (tipo == 4) tipo4(x_bis+120,i);
-        	else if (tipo == 5) tipo5(x_bis+120,i);
+        	else if (tipo == 2){
+        		int index = i;
+        		tiposPreg[i] = tipo;
+        		
+        		ArrayList<String> opcions = cp.getopcionest2(index);
+        		SpinnerListModel model = new SpinnerListModel(opcions);
+        		JSpinner spinner = new JSpinner(model);
+                spinner.setBounds(x+120, y, z, w);
+                spinner.addChangeListener(new ChangeListener() {
+        			public void stateChanged(ChangeEvent e) {
+        				sp2 = (String)spinner.getValue();
+        				respPreg[index] = sp2.toString(); //comprobar
+        				 System.out.println(respPreg[index]);
+        			}
+        		});
+                contentPane.add(spinner);
+        	}
+        	else if (tipo == 3) {
+        		int index = i;
+        		tiposPreg[i] = tipo;
+        		
+        		ArrayList<String> opcions = cp.getopcionest3(index);
+        		SpinnerListModel model = new SpinnerListModel(opcions);
+        		JSpinner spinner = new JSpinner(model);
+                spinner.setBounds(x+120, y, z, w);
+                spinner.addChangeListener(new ChangeListener() {
+        			public void stateChanged(ChangeEvent e) {
+        				sp3 = (String)spinner.getValue();
+        				respPreg[index] = sp3.toString(); //comprobar
+        				 System.out.println(respPreg[index]);
+        			}
+        		});
+                contentPane.add(spinner);
+        	}
+        	else if (tipo == 4) {
+        		tiposPreg[i] = tipo;
+        		items = cp.getopcionest4(i);
+        		for (int h = 0; h < items.size(); ++h) {
+        			int n = 0;
+        			selected.add(h,n);
+        			System.out.println("INICIALITZACIO");
+        		}
+        		
+        		int aux = x+120;
+        		int index = i;
+        		for (int m = 0; m < items.size(); ++m){
+        			System.out.println("SPINNER 4 " + m + " :"+ items.get(m).toString());
+        			JCheckBox chckbxNewCheckBox = new JCheckBox(items.get(m).toString());
+	                chckbxNewCheckBox.setBounds(aux, y, z, w);
+	                int n = m;
+	       
+	                chckbxNewCheckBox.addChangeListener(new ChangeListener() {
+	        			public void stateChanged(ChangeEvent e) {
+	        			
+	        				if(chckbxNewCheckBox.isBackgroundSet()){
+	        					System.out.println(selected.get(n));
+	        					if (selected.get(n)==0){
+	        						//System.out.println("SELECCIONADO");
+	        						selected.set(n,1);
+	        					}
+	        					else if (selected.get(n)==1){
+	        						selected.set(n,0);
+	        						//System.out.println("--- NO SELECCIONADO---");
+	        					}
+	        				}
+	        				
+	        				JButton btnOK = new JButton("OK Tipo 4");
+	        	            btnOK.addActionListener(new ActionListener() {
+	        	            	public void actionPerformed(ActionEvent e) {
+	        	            		String aux1 = "";
+	        	            		for (int l = 0; l < selected.size();++l){
+	        	            			if(selected.get(l)==1){
+	        	            				System.out.println(items.get(l));
+	        	            				if(aux1.isEmpty()) aux1 = items.get(l)+',';
+	        	            				else aux1+=items.get(l)+',';
+	        	            			}
+	        	            		}
+	        	            		System.out.println(aux1 + " " + index);
+	        	            		respPreg[index] = aux1;
+	        	            		 System.out.println(respPreg[index]);
+	        	            		
+	        	            	}
+	        	            });
+	        	            
+	        	            btnOK.setBounds(400,y , z, w);
+	        	            contentPane.add(btnOK);
+	        	            
+	        				System.out.println(selected);
+	        			}
+	        		});
+	                contentPane.add(chckbxNewCheckBox);
+	                aux+=100;
+	                
+        		}
+        		
+        	}
+        	else if (tipo == 5) {
+        		tiposPreg[i] = tipo;
+        		textField = new JTextField();
+                textField.setBounds(x+120, y, 200, w);
+                contentPane.add(textField);
+                textField.setColumns(10);
+                respPreg[i] = textField.toString();
+                System.out.println(respPreg[i]);
+        	}
         	
         }
+        
         JButton btnGuardar = new JButton("Guardar");
         btnGuardar.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		System.out.println("SPINNER VAL " + sp1); 
+        		
+        		for(int q = 0; q < tiposPreg.length;++q){
+        			Pregunta p = cp.getPreguntaiessima(q);
+        			if (tiposPreg[q] == 1){
+        				
+        				
+        			}
+        			if (tiposPreg[q] == 2){
+        				
+        			}
+        			if (tiposPreg[q] == 3){
+        				
+        			}
+        			if (tiposPreg[q] == 4){
+        				
+        			}
+        			if (tiposPreg[q] == 5){
+        				
+        			}
+        			
+        			
+        		}
+
+        		
         	}
         });
         btnGuardar.setBounds(173, 411, 117, 29);
         contentPane.add(btnGuardar);
-        //TODO GUARDAR INFO
         
         JButton btnAtras = new JButton("Atrás");
         btnAtras.addActionListener(new ActionListener() {
@@ -236,7 +302,6 @@ public class Frame_contestar extends JFrame implements ActionListener{
         });
         btnAtras.setBounds(49, 411, 117, 29);
         contentPane.add(btnAtras);
-        //TODO GUARDAR INFO
         
 	}
 
